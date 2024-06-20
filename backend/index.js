b38f3ac7754d6fdba3f1606fd6f431d9860d72a9
Aditya <code.adi8103@gmail.com>
2024-06-20 19:07:16 +0530

    upload funciton commited

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,34 @@
 const express = require("express");
 const connectToMongo = require("./Database/db");
 const errorHandler = require("./middleware/errorHandler");
+const multer = require("multer");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
 connectToMongo(process.env.URL);
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("resumeImage"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+
+  res.status(200).json({
+    message: "File uploaded successfully",
+    filePath: req.file.path,
+  });
+});
 app.use("/auth", require("./routes/Auth"));
 app.use("/profile", require("./routes/Profile"));
 app.use("/admin", require("./routes/Admin"));
